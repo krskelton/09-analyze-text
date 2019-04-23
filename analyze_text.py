25720289d7811c0fa92a50be9108cc5bf9fd8192
krskelton <kskelton82@gmail.com>
2019-04-22 21:32:44 -0600

    Update analyze_text.py

--- analyze_text.py
+++ analyze_text.py
@@ -1,4 +1,13 @@
-# Remember to reach out for help after your own due diligence
-
 def analyze_text(text):
-    # Your code here
+    # Your code here   
+    length_of_alpha_char = 0
+    
+    for char in text:
+        if char.isalpha() == True:
+            length_of_alpha_char += 1
+            
+    e_count = int(text.count('e')) + int(text.count('E'))
+    percent_of_e = (e_count / length_of_alpha_char) * 100
+    return_string = '''The text contains {0} alphabetic characters, of which {1} ({2}%) are 'e'.'''
+    
+    return return_string.format(length_of_alpha_char, e_count, percent_of_e)
