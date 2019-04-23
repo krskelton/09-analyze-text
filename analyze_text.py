2aff9d4284bd16400509d902b3f14ec25d7a5c48
krskelton <kskelton82@gmail.com>
2019-04-22 21:41:25 -0600

    Update analyze_text.py

--- analyze_text.py
+++ analyze_text.py
@@ -7,7 +7,7 @@ def analyze_text(text):
             length_of_alpha_char += 1
             
     e_count = int(text.count('e')) + int(text.count('E'))
-    percent_of_e = (e_count / length_of_alpha_char) * 100
+    percent_of_e = round((e_count / length_of_alpha_char) * 100, 2)
     return_string = '''The text contains {0} alphabetic characters, of which {1} ({2}%) are 'e'.'''
     
     return return_string.format(length_of_alpha_char, e_count, percent_of_e)
